b1f637365bea2aa34e4d56c3d1227ac69f205ef1
Jason Schorr <jason@spyglassltd.com>
2020-08-01 20:11:42 -0400

    1

--- startup.sh
+++ startup.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
 sudo service docker start
 sudo docker pull
