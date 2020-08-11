ba3886753ab78d211846ed0b4c230a1f562121de
Jason Schorr <jason@spyglassltd.com>
2020-08-10 22:46:08 -0400

    2

--- startup.sh
+++ startup.sh
@@ -15,8 +15,9 @@ sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
 sudo service docker start
 sudo docker pull docker.elastic.co/elasticsearch/elasticsearch-oss:7.0.1
-sudo docker pull appbaseio/dejavu
-sudo docker run -p 80:1358 -d appbaseio/dejavu
+#sudo docker pull appbaseio/dejavu
+#sudo docker run -p 80:1358 -d appbaseio/dejavu
+sudo docker run -p 80:8080  -d cars10/elasticvue
 
 EXTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 docker run -d --rm --name elasticsearch -p 9200:9200 -p 9300:9300 -e discovery.type=single-node -e http.cors.enabled=true -e http.cors.allow-origin=http://localhost:1358,http://127.0.0.1:1358,http://$EXTERNAL_IP -e http.cors.allow-headers=X-Requested-With,X-Auth-Token,Content-Type,Content-Length,Authorization -e http.cors.allow-credentials=true docker.elastic.co/elasticsearch/elasticsearch-oss:7.0.1
