4c9cc8edd2db1f1525f15a3368b9496af8d7a77a
Jason Schorr <echohtp@gmail.com>
2020-08-13 01:43:23 -0400

    Update sift_startup.sh

--- sift_startup.sh
+++ sift_startup.sh
@@ -1,8 +1,9 @@
-sudo apt-get update -y
+sudo apt-get update -y 
 sudo apt-get git gpg -y
-curl -Lo /usr/local/bin/sift https://github.com/sans-dfir/sift-cli/releases/download/v1.8.5/sift-cli-linux
-curl -Lo /tmp/sift.sha256.asc https://github.com/sans-dfir/sift-cli/releases/download/v1.8.5/sift-cli-linux.sha256.asc
-chmod +x /usr/local/bin/sift
+sudo curl -Lo /usr/local/bin/sift https://github.com/sans-dfir/sift-cli/releases/download/v1.8.5/sift-cli-linux
+sudo curl -Lo /tmp/sift.sha256.asc https://github.com/sans-dfir/sift-cli/releases/download/v1.8.5/sift-cli-linux.sha256.asc
+sudo chmod +x /usr/local/bin/sift
 gpg --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys 22598A94
 gpg --verify /tmp/sift.sha256.asc
 sift install
+
